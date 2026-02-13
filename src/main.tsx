362873125d2060cb21395b1a4eb5682fcdc59e64
KudoGDKN <vv080237@gmail.com>
2026-02-13 23:32:33 +0700

    Update base path for valentinesgift

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import Valentine from './route/valentine'
 import Valuser from './route/valuser'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <Valentine /> */}
     <Valuser />
   </StrictMode>,
 )
